55ce93128dc6fad1b90fa3ccc39f1af02d2a6c3f
shiva-charan-git <shivacharan021@gmail.com>
2023-03-03 15:02:19 +0530

    m

--- variables.sh
+++ variables.sh
@@ -3,7 +3,17 @@ echo "Hello, Good morning, Today date is ${DATE}"
 
 #####Cmd Substition
 
+### VAR=${cmd}
+
+#echo VAR
+
 
 DATE=$(date)
 
 echo "Hello, Good morning, Today date is ${DATE}"
+
+#Arthematic Substitution
+
+ADD=$((2+3))
+
+echo "2+3 = ${ADD}"
